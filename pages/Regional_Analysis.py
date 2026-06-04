06b54e059ad0d3bd25fdbad5275eb60158ec6e83
ashwinisugavi <ashwini.sagar1202@gmail.com>
2026-06-04 10:45:14 +0530

    Update Regional_Analysis.py

--- pages/Regional_Analysis.py
+++ pages/Regional_Analysis.py
@@ -1,1 +1,48 @@
 
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+st.title("🌍 Regional Analysis")
+
+df = pd.read_csv("startup_data.csv")
+
+region_funding = (
+    df.groupby("Region")
+    ["Funding Amount (M USD)"]
+    .sum()
+    .reset_index()
+)
+
+fig = px.bar(
+    region_funding,
+    x="Region",
+    y="Funding Amount (M USD)",
+    color="Region"
+)
+
+st.plotly_chart(fig,use_container_width=True)
+
+region_revenue = (
+    df.groupby("Region")
+    ["Revenue (M USD)"]
+    .sum()
+    .reset_index()
+)
+
+fig2 = px.line(
+    region_revenue,
+    x="Region",
+    y="Revenue (M USD)",
+    markers=True
+)
+
+st.plotly_chart(fig2,use_container_width=True)
+
+fig3 = px.sunburst(
+    df,
+    path=["Region","Industry"],
+    values="Revenue (M USD)"
+)
+
+st.plotly_chart(fig3,use_container_width=True)
