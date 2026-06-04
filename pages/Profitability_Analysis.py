f7fb23fab0a747f3bbadfdab1a9c60b173730a75
ashwinisugavi <ashwini.sagar1202@gmail.com>
2026-06-04 10:44:28 +0530

    Update Profitability_Analysis.py

--- pages/Profitability_Analysis.py
+++ pages/Profitability_Analysis.py
@@ -1,1 +1,47 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
 
+st.title("📈 Profitability Analysis")
+
+df = pd.read_csv("startup_data.csv")
+
+profit = (
+    df.groupby("Profitable")
+    .agg({
+        "Revenue (M USD)":"mean",
+        "Valuation (M USD)":"mean"
+    })
+    .reset_index()
+)
+
+fig = px.bar(
+    profit,
+    x="Profitable",
+    y=["Revenue (M USD)",
+       "Valuation (M USD)"],
+    barmode="group"
+)
+
+st.plotly_chart(fig,use_container_width=True)
+
+fig2 = px.pie(
+    df,
+    names="Profitable",
+    title="Profitability Distribution"
+)
+
+st.plotly_chart(fig2,use_container_width=True)
+
+st.subheader("Profitable Startups")
+
+profitable_df = df[df["Profitable"]==True]
+
+st.dataframe(
+    profitable_df[
+        ["Startup Name",
+         "Industry",
+         "Revenue (M USD)",
+         "Valuation (M USD)"]
+    ]
+)
