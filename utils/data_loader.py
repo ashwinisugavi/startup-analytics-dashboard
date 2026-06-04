73758621d53986e712db34217b141c53cd51c2a6
ashwinisugavi <ashwini.sagar1202@gmail.com>
2026-06-04 10:52:22 +0530

    Update data_loader.py

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -3,4 +3,24 @@ import streamlit as st
 
 @st.cache_data
 def load_data():
-    return pd.read_csv("startup_data.csv")
+
+    df = pd.read_csv("startup_data.csv")
+
+    # Remove duplicates
+    df.drop_duplicates(inplace=True)
+
+    # Fill missing values
+    numeric_cols = df.select_dtypes(include=['number']).columns
+
+    for col in numeric_cols:
+        df[col].fillna(df[col].median(), inplace=True)
+
+    return df
+
+
+def get_numeric_columns(df):
+    return df.select_dtypes(include=['number']).columns.tolist()
+
+
+def get_categorical_columns(df):
+    return df.select_dtypes(include=['object']).columns.tolist()
